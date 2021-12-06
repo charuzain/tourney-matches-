8cf77c912f9b7670339abb89472e530c64863538
charuzain <charuabhizain@gmail.com>
2021-12-05 21:12:25 -0500

    MatchList and PlayerList component added

---
+++ src/components/MatchList.js
@@ -0,0 +1,12 @@
+import Match from './Match';
+ import React from 'react'
+ 
+ export default function MatchList() {
+   return (
+     <section className="PlayerList MatchList">
+       <h1>Match List</h1>
+       <Match/>
+     </section>
+   )
+ }
+ 
---
+++ src/components/PlayerList.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import Player from './Player';
+
+export default function PlayerList() {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      <Player/>
+    </section>
+  )
+}
